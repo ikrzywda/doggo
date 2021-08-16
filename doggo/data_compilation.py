b102a504982e6e758c7713ade1cd1745bc27c783
ikrzywda <krzywda.igor@protonmail.com>
2021-08-16 13:52:13 +0200

    parsing csv to a tuple of list of records and dictionary of id's

---
+++ doggo/data_compilation.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+import csv
+import sys
+
+def timestr_to_minutes(timestr):
+    split_date = timestr.split(':')
+    time_in_minutes = int(split_date[0]) * 60 + int(split_date[1])
+    return time_in_minutes
+
+def sort_data(source_file):
+    sorted_data = list()
+    usr_ids = dict()
+    with open(source_file) as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            sorted_data.append((row[0], timestr_to_minutes(row[1])))
+            usr_ids[row[0]] = 0
+    sorted_data = sorted(sorted_data, key=lambda tup : tup[0])
+    return (sorted_data, usr_ids)
+
+def find_pairs(sorted_data):
+    # TODO: sum up time and assign it to according ID
+    records = sorted_data[0]
+    user_dict = sorted_data[1]
+
+
+if __name__ == '__main__':
+    data_packet = sort_data(sys.argv[1])
+    print(data_packet[0], data_packet[1])
