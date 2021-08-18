d2d3245789e5f3ac91e68e74ee2a3b77e85d99eb
ikrzywda <krzywda.igor@protonmail.com>
2021-08-18 16:07:41 +0200

    computing net walk time

--- doggo/data_compilation.py
+++ doggo/data_compilation.py
@@ -19,12 +19,22 @@ def sort_data(source_file):
     sorted_data = sorted(sorted_data, key=lambda tup : tup[0])
     return (sorted_data, usr_ids)
 
-def find_pairs(sorted_data):
+def compute_walk_times(sorted_data):
     # TODO: sum up time and assign it to according ID
     records = sorted_data[0]
     user_dict = sorted_data[1]
-
+    current_user = records[0]
+    for i in range(1, len(records)):
+        print(current_user)
+        if current_user is None:
+            current_user = records[i]
+            print(records[i])
+        elif current_user[0] == records[i][0]:
+            user_dict[current_user[0]] += records[i][1] - current_user[1]
+            current_user = None
+    return user_dict
 
 if __name__ == '__main__':
     data_packet = sort_data(sys.argv[1])
-    print(data_packet[0], data_packet[1])
+    print(compute_walk_times((data_packet[0], data_packet[1])))
+
