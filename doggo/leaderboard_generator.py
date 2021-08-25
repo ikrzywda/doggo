bad843f426928504ea6a4c4834a28675ba338a9d
ikrzywda <krzywda.igor@protonmail.com>
2021-08-25 10:23:47 +0200

    inserting values to an html file

---
+++ doggo/leaderboard_generator.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+import sys
+import csv
+
+def results_to_html_list(results_file):
+    html_list = '<ul>\r\n\t'
+    with open(results_file) as results:
+        results_csv = csv.reader(results)
+        for rec in results_csv:
+            html_list += ('<li> ' + rec[0] + ': ' + rec[1] 
+                    + ' hours and ' + rec[2] + ' minutes ' + '</li>\r\n\t')
+    html_list += '\r</ul>\r\n'
+    return html_list
+
+def insert_results_to_template(template_file, results, output):
+    with open(output, 'w') as output_file:
+        with open(template_file, 'r') as template:
+            for line in template:
+                if line == '<!--START-->\n':
+                    output_file.write(results)
+                else:
+                    output_file.write(line)
+
+
+if __name__ == '__main__':
+    results = results_to_html_list(sys.argv[1])
+    insert_results_to_template(sys.argv[2], results, 'inserted_values.html')
